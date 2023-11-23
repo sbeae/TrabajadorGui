787518c5b0aa82be176a9b59c5b86741160e19b6
sebas <sebasaraki@gmail.com>
2023-11-23 19:12:18 -0300

    as

---
+++ src/main/java/org/example/Prueba.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Prueba {
+    public static void main(String[] args) {
+        Calculadora(1,3);
+    }
+    private static int Calculadora(int a, int b) {
+        System.out.println(a + b);
+        return a + b;
+    }
+}
